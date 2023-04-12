3526cec2e19d70e3fb8989590103e50e3b694cc7
g-greatdevaks <anmolsachdeva@google.com>
2023-04-12 14:25:17 +0530

    fix: ssl_certificates usage for https target proxy
    Fixd ssl_certificates variable type to list(string)
    to access list of strings directly as input instead
    of casting a single string into list in the module.

--- samples/x-shared-vpc/main.tf
+++ samples/x-shared-vpc/main.tf
@@ -120,6 +120,6 @@ module "mig-l7xlb" {
   project_id      = module.service-project.project_id
   name            = "apigee-xlb"
   backend_migs    = [for _, mig in module.apigee-x-bridge-mig : mig.instance_group]
-  ssl_certificate = module.nip-development-hostname.ssl_certificate
+  ssl_certificate = [module.nip-development-hostname.ssl_certificate]
   external_ip     = module.nip-development-hostname.ip_address
 }
