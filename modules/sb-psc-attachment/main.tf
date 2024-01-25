fd88f0a4477725a782801bb1e566cbd6a2fce93b
rs1986x <r.salamanna@gmail.com>
2024-01-25 14:40:00 +0100

    feat: adding target server to sb-psc-attachment module

--- modules/sb-psc-attachment/main.tf
+++ modules/sb-psc-attachment/main.tf
@@ -32,3 +32,37 @@ resource "google_apigee_endpoint_attachment" "endpoint_attachment" {
   location               = var.region
   service_attachment     = google_compute_service_attachment.psc_service_attachment.id
 }
+
+
+resource "google_apigee_target_server" "target_server" {
+  for_each = var.target_servers
+
+  name        = each.value.name
+  description = "Target server for ${var.name} endpoint attachment"
+  env_id      = each.value.environment_id
+  protocol    = each.value.protocol
+  host        = google_apigee_endpoint_attachment.endpoint_attachment.host
+  port        = each.value.port
+  is_enabled  = each.value.enabled
+
+  dynamic "s_sl_info" {
+    for_each = each.value.s_sl_info != null ? [1] : []
+    content {
+      enabled                  = each.value.s_sl_info.enabled
+      client_auth_enabled      = each.value.s_sl_info.client_auth_enabled
+      key_store                = each.value.s_sl_info.key_store
+      key_alias                = each.value.s_sl_info.key_alias
+      trust_store              = each.value.s_sl_info.trust_store
+      ignore_validation_errors = each.value.s_sl_info.ignore_validation_errors
+      protocols                = each.value.s_sl_info.protocols
+      ciphers                  = each.value.s_sl_info.ciphers
+      dynamic "common_name" {
+        for_each = each.value.s_sl_info.common_name != null ? [1] : []
+        content {
+          value          = each.value.s_sl_info.common_name.value
+          wildcard_match = each.value.s_sl_info.common_name.wildcard_match
+        }
+      }
+    }
+  }
+}
--- modules/sb-psc-attachment/versions.tf
+++ modules/sb-psc-attachment/versions.tf
@@ -15,15 +15,15 @@
  */
 
 terraform {
-  required_version = ">= 1.1.0"
+  required_version = ">= 1.3.0"
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.20.0"
+      version = ">= 4.83, <6"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 4.20.0"
+      version = ">= 4.83, <6"
     }
   }
 }
